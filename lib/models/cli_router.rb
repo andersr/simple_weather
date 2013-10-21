508446613bcf243e633463c870afbd4b02a197c3
jnoconor <joe.oconor@gmail.com>
2013-10-20 20:29:41 -0400

    include summary in output, change minor formatting, move weather_data class to models folder, prep to accept location argument

--- lib/models/cli_router.rb
+++ lib/models/cli_router.rb
@@ -3,22 +3,33 @@ class CLIRouter
   attr_reader :weather_forecast, :args
 
   def initialize(args)
-    @weather_forecast = Forecast.new(WeatherData.new)
     @args = args # in fact this is ARGV
+    
+    local_geo = Geocoder.coordinates(open('http://whatismyip.akamai.com').read)
+    @geo = Geocoder.coordinates(args[1]) || local_geo
+
+    wd = WeatherData.new(@geo.first, @geo.last)
+    @weather_forecast = Forecast.new(wd)
   end
 
   def help
-    weather_forecast.today
+    puts weather_forecast.today
+    puts weather_forecast.separator
     puts "if you'd like a different forecast try something like 'forecast tomorrow'"
   end
 
   def commands
     if args.length == 1
       puts weather_forecast.send(self.args[0])
+    elsif args.length == 2
+      weather_forecast.compare_loc = args[1]
+      puts "I'm comparing against #{weather_forecast.compare_loc}"
+      puts weather_forecast.send(self.args[0])
     elsif args.length == 0
       help
     end
   end
+
 
 
   # def this_weekend
--- lib/models/forecast.rb
+++ lib/models/forecast.rb
@@ -1,36 +1,51 @@
 class Forecast
 
-  attr_accessor :today_temp, :yesterday_temp, :tomorrow_temp, :weather_data
+  attr_accessor :today_temp, :yesterday_temp, :tomorrow_temp, :weather_data, :compare_loc
 
   def initialize(weather_data_object)
     @weather_data = weather_data_object
 
-    @today_temp = self.weather_data.temp_today
-    @tomorrow_temp = self.weather_data.temp_tomorrow
+    @today_temp = self.weather_data.today_temp
+    @tomorrow_temp = self.weather_data.tomorrow_temp
     # @this_weekend = self.avg_temp_this_weekend
     # @next_week = self.avg_temp_next_week
   end
 
   def today
     puts "hmm, interesting!"
-    @yesterday_temp = self.weather_data.temp_yesterday
-    compare(today_temp, yesterday_temp) + "yesterday"
+    @yesterday_temp = self.weather_data.yesterday_temp
+    separator + 
+    "temp: #{compare(today_temp, yesterday_temp)}yesterday\n#{self.weather_data.today_summary}"
   end
 
   def tomorrow
-    compare(tomorrow_temp, today_temp) + "today"
+    separator +
+    "temp: #{compare(tomorrow_temp, today_temp)}today\n#{self.weather_data.tomorrow_summary}"
   end
 
   def avg_temp_this_weekend
+    data = find_weekend_time # if it's 3, then take 1 and 2
+    weekend_avg = (data[1]["temperatureMax"] + data[2]["temperatureMax"])/2
+    puts weekend_avg
+    weekend_avg
+  end
+
+  def find_weekend_time
+    weekend_data = weather_data.today_data["daily"]["data"].select do |data_hash|
+      ruby_datetime = Time.at(data_hash["time"])
+      ruby_datetime.saturday? || ruby_datetime.sunday?
+    end
 
   end
+
 
   def avg_temp_next_weekend
 
   end
 
   def this_weekend
-    # compare(this_week_avg_temp?, temp_for_upcoming_weekend)
+    puts today_temp
+    compare(avg_temp_this_weekend, today_temp) + "today"
   end
 
   def next_week
@@ -38,10 +53,10 @@ class Forecast
   end
 
   def compare(temp1, temp2)
-    diff = temp1-temp2
+    diff = (temp1.round-temp2.round).abs
 
-    case diff.abs
-    when (1..2)
+    case diff
+    when (0..2)
       temp1 = temp2
     when (3..7)
       mod = "slightly "
@@ -62,7 +77,10 @@ class Forecast
     end
   end
 
-
+  def separator
+    "------------------\n"
+  end
+  
 # categorization
 # "same"
 # "colder than"
