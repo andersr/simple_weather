159ab4feb22656b1eb16f0596eb8f640b73d12b6
Anders Ramsay <andersr@gmail.com>
2013-10-21 09:07:26 -0400

    removed scraper.rb

---
+++ lib/models/scraper.rb
@@ -0,0 +1,20 @@
+class Scraper
+  attr_reader :forecast
+
+  def initialize(scrape_target)
+    @scraped_content = Nokogiri::HTML(open(scrape_target))
+  end
+
+  def get_temp_yesterday
+    @scraped_content.css('p.wx-temp').text.strip[0..1].to_i
+  end
+
+   def get_temp_now
+    @scraped_content.css('span.temperature-fahrenheit')
+  end
+
+  def get_temp_tomorrow
+    @scraped_content.css('p.wx-temp').text.strip[0..1].to_i
+  end
+
+end
